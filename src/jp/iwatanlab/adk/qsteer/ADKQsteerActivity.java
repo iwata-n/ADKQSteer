c547f396ec82101fb33fdbbd8c1865a8551075f4
naoki <a.tokai.wain@gmail.com>
2011-10-27 22:36:38 +0900

    初回コミット

---
+++ src/jp/iwatanlab/adk/qsteer/ADKQsteerActivity.java
@@ -0,0 +1,126 @@
+/**
+ * 
+ */
+
+package jp.iwatanlab.adk.qsteer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ToggleButton;
+
+/**
+ * @author naoki
+ */
+public class ADKQsteerActivity extends BaseActivity implements OnTouchListener {
+
+    private class CtrlData {
+        public byte normal = 0;
+        public byte turbo = 0;
+
+        public CtrlData(byte normal, byte turbo) {
+            this.normal = normal;
+            this.turbo = turbo;
+        }
+    }
+
+    /** Logのタグ */
+    private static final String TAG = ADKQsteerActivity.class.getSimpleName();
+
+    private static final byte CTRL_STOP = 0;
+    private static final byte CTRL_FORWARD = 1;
+    private static final byte CTRL_BACK = 2;
+    private static final byte CTRL_LEFT = 3;
+    private static final byte CTRL_RIGHT = 4;
+    private static final byte CTRL_TURBO_FORWARD = 5;
+    private static final byte CTRL_FORWARD_LEFT = 6;
+    private static final byte CTRL_FORWARD_RIGHT = 7;
+    private static final byte CTRL_TURBO_FORWARD_LEFT = 8;
+    private static final byte CTRL_TURBO_FORWARD_RIGHT = 9;
+    private static final byte CTRL_LEFT_BACK = 10;
+    private static final byte CTRL_RIGHT_BACK = 11;
+    private static final byte CTRL_TURBO_BACK = 12;
+    private static final byte CTRL_TURBO_LEFT_BACK = 13;
+    private static final byte CTRL_TURBO_RIGHT_BACK = 14;
+
+    private List<CtrlData> ctrlData = new ArrayList<CtrlData>();
+
+    private ToggleButton tglTurbo;
+
+    private byte mBand = 0;
+
+    private void sendCommand(int id) {
+        CtrlData ctrl = ctrlData.get(id);
+        byte command = ctrl.normal;
+        if (tglTurbo.isChecked()) {
+            command = ctrl.turbo;
+        }
+        byte[] data = {
+                mBand, command
+        };
+        try {
+            write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setTouchEvent(int id) {
+        View v = findViewById(id);
+        v.setOnTouchListener(this);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        tglTurbo = (ToggleButton) findViewById(R.id.tgl_turbo);
+
+        ctrlData.add(new CtrlData(CTRL_STOP, CTRL_STOP));
+        ctrlData.add(new CtrlData(CTRL_FORWARD, CTRL_TURBO_FORWARD));
+        ctrlData.add(new CtrlData(CTRL_FORWARD_RIGHT, CTRL_TURBO_FORWARD_RIGHT));
+        ctrlData.add(new CtrlData(CTRL_RIGHT, CTRL_RIGHT));
+        ctrlData.add(new CtrlData(CTRL_RIGHT_BACK, CTRL_TURBO_RIGHT_BACK));
+        ctrlData.add(new CtrlData(CTRL_BACK, CTRL_TURBO_BACK));
+        ctrlData.add(new CtrlData(CTRL_LEFT_BACK, CTRL_TURBO_LEFT_BACK));
+        ctrlData.add(new CtrlData(CTRL_LEFT, CTRL_LEFT));
+        ctrlData.add(new CtrlData(CTRL_FORWARD_LEFT, CTRL_TURBO_FORWARD_LEFT));
+
+        setTouchEvent(R.id.btn_forward);
+        setTouchEvent(R.id.btn_right_forward);
+        setTouchEvent(R.id.btn_right);
+        setTouchEvent(R.id.btn_right_back);
+        setTouchEvent(R.id.btn_back);
+        setTouchEvent(R.id.btn_left_back);
+        setTouchEvent(R.id.btn_left);
+        setTouchEvent(R.id.btn_left_forward);
+        setTouchEvent(R.id.btn_stop);
+
+    }
+
+    public void onClickCtrl(View v) {
+        // int id = Integer.parseInt((String) v.getTag());
+        // sendCommand(id);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        int id = Integer.parseInt((String) v.getTag());
+
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            sendCommand(0);
+        } else {
+            sendCommand(id);
+        }
+
+        return false;
+    }
+}
--- src/jp/iwatanlab/adk/qsteer/BaseActivity.java
+++ src/jp/iwatanlab/adk/qsteer/BaseActivity.java
@@ -6,9 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import com.android.future.usb.UsbAccessory;
-import com.android.future.usb.UsbManager;
-
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -16,42 +13,42 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
-import android.widget.TextView;
 
-public class ADKQSteerActivity extends Activity implements Runnable {
-    private static final String TAG = ADKQSteerActivity.class.getSimpleName();
+import com.android.future.usb.UsbAccessory;
+import com.android.future.usb.UsbManager;
 
-    private static final int MSG_UPDATE_TEXT = 0;
+/**
+ * ADKを使用してチョロQを操作する
+ * 
+ * @author Iwata Naoki
+ */
+public class BaseActivity extends Activity implements Runnable {
 
+    /** Logのタグ */
+    private static final String TAG = BaseActivity.class.getSimpleName();
+
+    /** パーミッション */
     private static final String ACTION_USB_PERMISSION = "com.google.android.DemoKit.action.USB_PERMISSION";
 
-    private UsbManager mUsbManager;
+    /** パーミッション確認のインテント */
     private PendingIntent mPermissionIntent;
     private boolean mPermissionRequestPending;
 
+    /** USBマネージャー */
+    private UsbManager mUsbManager;
+
+    /** 現在通信中のUSBアクセサリ */
     private UsbAccessory mAccessory;
+
+    Thread thread = new Thread(null, this, "DemoKit");
+    private boolean mRunFlag = false;
     private ParcelFileDescriptor mFileDescriptor;
     private FileInputStream mInputStream;
     private FileOutputStream mOutputStream;
 
-    private TextView textViewLog;
-
-    private Handler handle = new Handler() {
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case MSG_UPDATE_TEXT:
-                    String str = String.valueOf(msg.obj);
-                    textViewLog.setText(str);
-                    break;
-            }
-
-        }
-    };
-
+    /** ブロードキャストレシーバー */
     private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -78,27 +75,47 @@ public class ADKQSteerActivity extends Activity implements Runnable {
         }
     };
 
+    /**
+     * USBアクセサリとの通信を開始
+     * 
+     * @param accessory
+     */
     private void openAccessory(UsbAccessory accessory) {
+
         mFileDescriptor = mUsbManager.openAccessory(accessory);
+
         if (mFileDescriptor != null) {
             mAccessory = accessory;
+
+            /* 通信用のファイルディスクリプタを開く */
             FileDescriptor fd = mFileDescriptor.getFileDescriptor();
             mInputStream = new FileInputStream(fd);
             mOutputStream = new FileOutputStream(fd);
-            Thread thread = new Thread(null, this, "DemoKit");
+
+            /* ADKからデータを受信するスレッドを生成 */
+            mRunFlag = true;
+            thread = new Thread(null, this, "DemoKit");
             thread.start();
             Log.d(TAG, "accessory opened");
-            textViewLog.setText("accessory opened");
             enableControls(true);
         } else {
             Log.d(TAG, "accessory open fail");
-            textViewLog.setText("accessory open fail");
         }
     }
 
+    /**
+     * ADKとの通信を終了
+     */
     private void closeAccessory() {
         enableControls(false);
         Log.d(TAG, "closeAccessory");
+        mRunFlag = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e1) {
+            Thread.interrupted();
+        }
+
         try {
             if (mFileDescriptor != null) {
                 mFileDescriptor.close();
@@ -110,23 +127,38 @@ public class ADKQSteerActivity extends Activity implements Runnable {
         }
     }
 
+    /**
+     * @param enable
+     */
     protected void enableControls(boolean enable) {
     }
 
-    /** Called when the activity is first created. */
+    public void write(byte[] data) throws IOException {
+        mOutputStream.write(data);
+    }
+
+    public void onReceived(byte[] data, int size) {
+
+    }
+
+    /**
+     * アプリ開始時にコールされる。USBマネージャーのインスタンス取得、Intentを設定し、
+     * USBアクセサリが接続された際のIntentを取得するブロードキャストレシーバーを設定する
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        Log.d(TAG, "onCreate");
 
-        textViewLog = (TextView) findViewById(R.id.textViewLog);
-
+        /* USBマネージャーのインスタンスを取得 */
         mUsbManager = UsbManager.getInstance(this);
+
+        /* アクセサリに関するIntentを設定 */
         mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
                 ACTION_USB_PERMISSION), 0);
         IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
         filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+
+        /** ブロードキャストレシーバーの登録 */
         registerReceiver(mUsbReceiver, filter);
 
         if (getLastNonConfigurationInstance() != null) {
@@ -134,26 +166,38 @@ public class ADKQSteerActivity extends Activity implements Runnable {
             openAccessory(mAccessory);
         }
 
-        enableControls(false);
     }
 
+    /**
+     * アプリがレジューム時にコールされる。USBアクセサリへのアクセスを開始する
+     */
     @Override
     public void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume");
 
-        Intent intent = getIntent();
+        /* すでにストリームが開かれていた場合は何もしない */
         if (mInputStream != null && mOutputStream != null) {
             Log.d(TAG, "Stream Opened");
             return;
         }
 
+        /* 接続されているUSBアクセサリのリストを取得 */
         UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+
+        /* 接続されているUSBアクセサリが無ければnull、１つ以上あれば最初の１つを取得 */
         UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+
         if (accessory != null) {
+            /* USBアクセサリにアクセスするパーミッションがあるか確認 */
             if (mUsbManager.hasPermission(accessory)) {
+                Log.d(TAG, "hasPermission");
+
+                /* USBアクセサリへのアクセスを開始 */
                 openAccessory(accessory);
             } else {
+                Log.d(TAG, "not hasPermission");
+
+                /* パーミッションの要求 */
                 synchronized (mUsbReceiver) {
                     if (!mPermissionRequestPending) {
                         mUsbManager.requestPermission(accessory,
@@ -163,44 +207,49 @@ public class ADKQSteerActivity extends Activity implements Runnable {
                 }
             }
         } else {
+            /* USBアクセサリが接続されていないか、認識できていない */
             Log.d(TAG, "mAccessory is null");
         }
     }
 
+    /**
+     * アプリがポーズ時にコールされる。USBアクセサリへのアクセスを終了する
+     */
     @Override
     public void onPause() {
         super.onPause();
         closeAccessory();
     }
 
+    /**
+     * アプリが終了時にコールされる。 USBアクセサリが接続された際のIntentを取得するブロードキャストレシーバーを解除する
+     */
     @Override
     public void onDestroy() {
         unregisterReceiver(mUsbReceiver);
         super.onDestroy();
     }
 
+    /**
+     * USBアクセサリからのデータを受信するスレッド
+     */
     @Override
     public void run() {
 
         int ret = 0;
         byte[] buffer = new byte[16384];
-        int i;
-        Log.d(TAG, "Thread run");
 
-        while (ret >= 0) {
+        Log.d(TAG, "Thread start");
+
+        while (mRunFlag) {
+
             try {
-                mOutputStream.write("test".getBytes());
-                Log.d(TAG, "wait read");
                 ret = mInputStream.read(buffer);
-                String str = new String(buffer);
-                Log.d(TAG, str);
-                Message msg = handle.obtainMessage();
-                msg.what = MSG_UPDATE_TEXT;
-                msg.obj = str;
-                handle.sendMessage(msg);
+                onReceived(buffer, ret);
             } catch (IOException e) {
-                break;
+                e.printStackTrace();
             }
+
         }
     }
 }
